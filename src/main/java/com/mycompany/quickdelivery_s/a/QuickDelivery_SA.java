68ccd06ce3938d847c7ad17e814c2adb7e8415c6
Yemorales <yedelin71@gmail.com>
2026-03-29 10:35:43 -0600

    Primer Commit

---
+++ src/main/java/com/mycompany/quickdelivery_s/a/QuickDelivery_SA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quickdelivery_s.a;
+
+/**
+ *
+ * @author yedel
+ */
+public class QuickDelivery_SA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
